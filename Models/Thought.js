d27bf8431e57713915f3e13f2a88241d8b2c3e53
Mona Lisa <cjensen@netechnical.com>
2023-03-07 07:38:13 -0600

    added unique validator in User model, and virtual property in Thought model

--- Models/Thought.js
+++ Models/Thought.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
 
 const thoughtSchema = new mongoose.Schema ({
-    //need to be between 1-280 characters
-    thoughtText: { type: String, required: true },
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
     //use a getter method to format the timestamp on query
     createAt: { type: Date, default: Date.now },
     username: { type: String, required: true },
-    //add in reactions(like replies) reactions: {},
+    reactions: [{ type: Schema.Types.ObjectId, ref: 'thought' }],
     //array of nested documents created with the reactionSchema
-});
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
 
 //schema settings: create virtual called reactionCount that retreives the length of the thought's reactions array field on query
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
 
+const Thought = model('thought', thoughtSchema);
 
 module.exports = Thought;
--- Models/User.js
+++ Models/User.js
@@ -1,13 +1,39 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema ({
-    //need to add in unique and trimmed
-    username: { type: String, required: true },
+    //need to add in trimmed
+    username: { type: String, required: true, unique: true },
     //need to add in unique and matching validation
     email: { type: String, required: true },
     //reference thought model: thoughts: { array: },
     //reference User model: friends; {}
 });
+
+// const U1 = db.model('U1', uniqueUsernameSchema);
+// const U2 = db.model('U2', uniqueUsernameSchema);
+// const dup = [{ username: 'Val' }, { username: 'Val' }];
+// // Race condition! This may save successfully, depending on whether
+// // MongoDB built the index before writing the 2 docs.
+// U1.create(dup).
+//   then(() => {
+//   }).
+//   catch(err => {
+// });
+// // You need to wait for Mongoose to finish building the `unique`
+// // index before writing. You only need to build indexes once for
+// // a given collection, so you normally don't need to do this
+// // in production. But, if you drop the database between tests,
+// // you will need to use `init()` to wait for the index build to finish.
+// U2.init().
+//   then(() => U2.create(dup)).
+//   catch(error => {
+//     // `U2.create()` will error, but will *not* be a mongoose validation error, it will be
+//     // a duplicate key error.
+//     // See: https://masteringjs.io/tutorials/mongoose/e11000-duplicate-key
+//     assert.ok(error);
+//     assert.ok(!error.errors);
+//     assert.ok(error.message.indexOf('duplicate key error') !== -1);
+//   });
 
 //schema settings: create virtual called friendCount that retreives the length of the user's friends array field on query
 
