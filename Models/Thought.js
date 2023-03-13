923e22ee37c22f69f2af58bfbde0a178d64eae06
Mona Lisa <cjensen@netechnical.com>
2023-03-13 12:25:38 -0500

    updated code for formated date

--- Models/Thought.js
+++ Models/Thought.js
@@ -1,12 +1,12 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
 //add this into utils-look at activities
-const dateformat = require('../utils/dateformat')
+const format_date = require('../utils/dateformat')
 
 const thoughtSchema = new Schema ({
     thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
     //use a getter method to format the timestamp on query: run date format
-    createdAt: { type: Date, default: Date.now, get: timestamp => dateformat(timestamp) },
+    createdAt: { type: Date, default: Date.now, get: timestamp => format_date(timestamp) },
     username: { type: String, required: true },
     reactions: [reactionSchema],
 },
