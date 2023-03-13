667edf9c205ac441b4e5b01328a3cb403161fc7c
Mona Lisa <cjensen@netechnical.com>
2023-03-13 16:02:56 -0500

    updated getter so it was inside my reactionSchema

--- Models/Reaction.js
+++ Models/Reaction.js
@@ -8,14 +8,9 @@ const reactionSchema = new Schema ({
     reactionBody: { type: String, required: true, maxlength: 280 },
     username: { type: String, required: true },
     createdAt: { type: Date, default: Date.now, get: timestamp => dateformat(timestamp) },
-});
     //use a getter method to format the timestamp on query
-{
-    toJSON: {
-        getters: true
-    }
-    id: false
-};
+    toJSON: { getters: true, id: false }
+});
 
 
 //schema settings: this will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model
